93aa0c201660a8be696a225ed091484ca34b14ce
aakaaa <124658090+aakaaa@users.noreply.github.com>
2025-04-23 15:43:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  publicDir: 'public', 
   base: process.env.NODE_ENV === 'production' ? '/file-tools-website/' : '/',
   build: {
     outDir: 'docs'
