61eb136bad1067b17bd7fbde9d86a3a986140b1e
aakaaa <124658090+aakaaa@users.noreply.github.com>
2025-04-23 15:19:02 +0800

    Create ToolCard.vue

---
+++ src/components/ToolCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tool-card" @click="navigateToTool">
+    <div class="icon">{{ icon }}</div>
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'icon', 'component'],
+  methods: {
+    navigateToTool() {
+      this.$router.push({ name: this.component })
+    }
+  }
+}
+</script>
